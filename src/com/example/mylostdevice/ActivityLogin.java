2641036e40b1eccfa5800573375553782c976309
rgap <rgap449@gmail.com>
2013-05-19 22:16:30 -0500

    other....

--- src/com/example/mylostdevice/ActivityLogin.java
+++ src/com/example/mylostdevice/ActivityLogin.java
@@ -14,6 +14,18 @@ import android.content.Context;
 
 import android.widget.ProgressBar;
 import android.widget.Toast;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by rgap on 18/05/13.
@@ -24,6 +36,8 @@ public class ActivityLogin extends Activity{
     private EditText loginpass;
 
     private ProgressBar pb;
+
+    int userid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,12 +63,31 @@ public class ActivityLogin extends Activity{
 
     public void actionLogin(View v){
 
+        Thread TLoginUsu = new Thread(new CheckLoginRunnable(loginemail.getText().toString(),loginpass.getText().toString()));
+        TLoginUsu.start();
 
-        Intent i=new Intent(this,ActivityTabs.class);
-        startActivity(i);
+        while(TLoginUsu.isAlive());
 
-        tToast("Login Successful");
-        //tToast("Device Registered");
+        if(userid == 0){
+            tToast("Incorrect Data");
+            return;
+        }
+        else{
+
+            SharedPreferences preferences = getSharedPreferences("PrefFile", MODE_PRIVATE);
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putInt("userid",  userid);
+            editor.commit();
+
+            Intent i=new Intent(this,ActivityTabs.class);
+            startActivity(i);
+
+            tToast("Login Successful");
+        }
+
+
+
     }
 
     public void actionGoRegister(View v){
@@ -63,6 +96,54 @@ public class ActivityLogin extends Activity{
         startActivity(i);
 
         tToast("Going to register page");
+    }
+
+    private class CheckLoginRunnable implements Runnable {
+
+        String email,pass;
+
+        CheckLoginRunnable(String email, String pass)
+        {
+            this.email=email;
+            this.pass=pass;
+        }
+
+        @Override
+        public void run() {
+
+            HttpClient httpclient=new DefaultHttpClient();
+            HttpPost httppost =new HttpPost("http://192.168.1.34/MyLostDevice/controller_client/corUserCheck.php");
+
+            try {
+                // Add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("username", email));
+                nameValuePairs.add(new BasicNameValuePair("password", pass));
+
+
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+
+                ByteArrayOutputStream out=new ByteArrayOutputStream();
+                response.getEntity().writeTo(out);
+                String payload=out.toString();
+
+                JSONObject obj=new JSONObject(payload);
+
+
+                userid =  Integer.parseInt(obj.getString("resp"));
+
+
+                Log.e("addReg", obj.getString("resp"));
+                //Log.e("addReg", String.valueOf(preferences.getInt("userid",0)));
+
+            } catch (Exception e){
+
+                Log.e("addReg", "Exception " + e.toString());
+            }
+        }
     }
 
     private void tToast(String s) {
