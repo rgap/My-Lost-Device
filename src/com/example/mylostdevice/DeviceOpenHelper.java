d566696a4fc94028f273b60d4429d4f031354da9
rgap <rgap449@gmail.com>
2013-05-30 21:16:05 -0500

    last one

--- src/com/example/mylostdevice/DeviceOpenHelper.java
+++
@@ -1,7 +0,0 @@
-package com.example.mylostdevice;
-
-/**
- * Created by rgap on 18/05/13.
- */
-public class DeviceOpenHelper {
-}
--- src/com/example/mylostdevice/models/Device.java
+++
@@ -1,8 +0,0 @@
-package com.example.mylostdevice.models;
-
-/**
- * Created by rgap on 18/05/13.
- */
-public class Device {
-
-}
